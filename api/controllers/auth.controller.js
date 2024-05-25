1257e685cfeb3da610996ed4be7061c9e64c3c22
apply007 <hasanat153@gmail.com>
2024-05-26 00:11:01 +0600

    create middleware and function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,27 +1,24 @@
+import User from "../models/user.model.js";
 
-import User from '../models/user.model.js'
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-import bcryptjs from 'bcryptjs'
+export const signup = async (req, res, next) => {
+  ///
 
+  const { userName, email, password } = req.body;
 
+  const hashPassword = bcryptjs.hashSync(password, 10);
 
-export const signup=async (req,res)=>{
-
-///
-
-
-const {userName,email,password}=req.body
-
-const hashPassword= bcryptjs.hashSync(password,10)
-
-const newUser=new User({
-    userName,email,password:hashPassword
-})
-try {
-    await newUser.save()
-res.status(200).json({message:"User save successfully"})
-} catch (error) {
-    res.status(500).json({msg:error.message})
-}
-
-}
+  const newUser = new User({
+    userName,
+    email,
+    password: hashPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(200).json({ message: "User save successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
@@ -20,9 +20,21 @@ app.listen(3000, (req, res) => {
   console.log(`connected successfully at port ${3000}!!! `);
 });
 
+app.use(express.json());
 
-app.use(express.json())
 
-app.use("/api/user",userRouter)
 
-app.use("/api/auth",authRouter)
+app.use("/api/user", userRouter);
+
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
