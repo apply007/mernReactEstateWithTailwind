9cf640a914827b1ad47c7a305e54c57543b1ee21
apply007 <hasanat153@gmail.com>
2024-06-01 14:04:30 +0600

    Create user update API

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
@@ -22,7 +23,7 @@ app.listen(3000, (req, res) => {
 
 app.use(express.json());
 
-
+app.use(cookieParser())
 
 app.use("/api/user", userRouter);
 
