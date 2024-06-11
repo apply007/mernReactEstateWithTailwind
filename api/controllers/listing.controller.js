32a590a0b60e4f5f9326ec59d08e49a7ababd19a
apply007 <hasanat153@gmail.com>
2024-06-12 01:24:00 +0600

    delete listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,12 +1,34 @@
-import listing from "../models/listing.model.js"
+import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js"
 
 
 
 export const createListing=async(req,res,next)=>{
 try {
-   const listings= await listing.create(req.body)
+   const listings= await Listing.create(req.body)
+   console.log(req.body)
    return res.status(201).json(listings)
 
+} catch (error) {
+    next(error)
+}
+}
+
+// delete listing
+export const deleteListing=async(req,res,next)=>{
+try {
+   const listings= await Listing.findById(req.params.id)
+console.log(req)
+if (!listings) {
+    return next(errorHandler(404,"listings not found"))
+}
+
+if (req.user.id !=listings.userRef) {
+    return next(errorHandler(404,"You can delete your own listings"))
+    
+}
+   await Listing.findByIdAndDelete(req.params.id)
+   res.status(200).json("Listing deleted")
 } catch (error) {
     next(error)
 }
