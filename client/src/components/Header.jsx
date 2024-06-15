a23c54666f9f006d050d259342dd4b7536d298f6
apply007 <hasanat153@gmail.com>
2024-06-15 15:03:21 +0600

    update header search

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
-const {currentUser}=useSelector((state)=>state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    urlParams.set("searchTerm", searchTerm);
+
+    const searchQuery = urlParams.toString();
+
+    navigate(`/search?${searchQuery}`);
+  };
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -17,32 +39,46 @@ const {currentUser}=useSelector((state)=>state.user)
             <span className="text-slate-800">Hasan</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex justify-center items-center ">
+        <form
+          className="bg-slate-100 p-3 rounded-lg flex justify-center items-center "
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             placeholder="Search.."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button type="submit">
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
 
         <ul className="flex gap-4 font-semibold">
-          <Link to={'/'}>
+          <Link to={"/"}>
             {" "}
             <li className="hidden sm:inline  text-slate-500 hover:text-slate-800 hover:underline">
               Home
             </li>
           </Link>
-          <Link to={'/about'}>
+          <Link to={"/about"}>
             <li className=" text-slate-500 hover:text-slate-800 hover:underline">
               About
             </li>
           </Link>
-          <Link to={'/profile' }>
-            {currentUser? (<img src={currentUser.avatar} alt="" className="rounded-full h-7 w-7 object-cover"/>) :( <li className=" text-slate-500 hover:text-slate-800 hover:underline">
-              Sign In
-            </li>)}
-           
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt=""
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className=" text-slate-500 hover:text-slate-800 hover:underline">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
